5cd0e334e4abdacd05909dcbf39ad8435da95d4a
armand-hoxha25 <armand.hoxha25@gmail.com>
2020-07-21 17:12:43 -0400

    get player dataframe

--- get_all_match_ids_opendota.py
+++ get_all_match_ids_opendota.py
@@ -1,8 +1,10 @@
 import requests
 import pickle
-
-api_req='''https://api.opendota.com/api/players/78812268/matches?limit=6000&project=heroes'''
-r=requests.get(api_req)
+import sys 
+import pandas as pd
+account_id=78812268 #32 bit steam id
+api_req='''https://api.opendota.com/api/players/{}/matches?limit=6000&project=heroes'''
+r=requests.get(api_req.format(account_id))
 matchhistory=r.json()
 
 pickle.dump(matchhistory,open('armand_matches.p','wb'))
@@ -11,7 +13,34 @@ pickle.dump(matchhistory,open('armand_matches.p','wb'))
 
 ## parse open dota data
 match_ids=[]
-heroes_played=[]
+heroes=[]
 match_length=[]
 win=[]
 team_radiant=[] # player_slot < 128 is radiant
+l=len(matchhistory)
+n=0
+for match in matchhistory:
+    match_ids.append(match['match_id'])
+    playersheroes=list(match['heroes'].values())
+    for d in playersheroes:
+        try:
+            if d['account_id']==account_id:
+                break
+        except KeyError:
+            pass
+    heroes.append(d['hero_id'])
+    match_length.append(match['duration'])
+    win.append(True if match['radiant_win'] and match['player_slot']<128 else False)
+    team_radiant.append(True if match['player_slot']<128 else False)
+    n+=1
+    x=str(n)+'/'+str(l)
+    print(f'\r', end="")
+    print(f'{x}', end="")
+print(f'\n')
+
+player_df=pd.DataFrame({'match_ids':match_ids,
+'heroes':heroes,
+'match_length':match_length,
+'win':win,
+'team_radiant':team_radiant})
+
