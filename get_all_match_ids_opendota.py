fbedaafde2473d4b60f8e22e535b90089f2adf32
armand-hoxha25 <armand.hoxha25@gmail.com>
2020-07-21 12:42:45 -0400

    updated pickle line

--- get_all_match_ids_opendota.py
+++ get_all_match_ids_opendota.py
@@ -1,8 +1,17 @@
 import requests
 import pickle
 
-api_req='''https://api.opendota.com/api/players/78812268/matches'''
+api_req='''https://api.opendota.com/api/players/78812268/matches?limit=6000&project=heroes'''
 r=requests.get(api_req)
 matchhistory=r.json()
 
-pickle.dump(j,open('armand_matches.p','wb'))
+pickle.dump(matchhistory,open('armand_matches.p','wb'))
+
+
+
+## parse open dota data
+match_ids=[]
+heroes_played=[]
+match_length=[]
+win=[]
+team_radiant=[] # player_slot < 128 is radiant
