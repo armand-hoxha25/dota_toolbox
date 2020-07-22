ab555936abbdd52729a364fa555e142316a10756
armand-hoxha25 <armand.hoxha25@gmail.com>
2020-07-22 18:20:00 -0400

    added documentations

--- get_all_match_ids_opendota.py
+++ get_all_match_ids_opendota.py
@@ -3,44 +3,72 @@ import pickle
 import sys 
 import pandas as pd
 account_id=78812268 #32 bit steam id
-api_req='''https://api.opendota.com/api/players/{}/matches?limit=6000&project=heroes'''
-r=requests.get(api_req.format(account_id))
-matchhistory=r.json()
+def get_player_match_history(account_id):
+    '''
+    Fetches the last 10,000 matches of the player, and saves it into a pickle file (due to limited
+    number of api calls in each month)
+    
+    Input:
+    account_id (int): the Dota Id or the Steam 32 bit ID
 
-pickle.dump(matchhistory,open('mike_matches.p','wb'))
+    Output:
+    matchhistory (dict): a list of dictionaries of each match played with the opendota format
+    '''
+    api_req='''https://api.opendota.com/api/players/{}/matches?limit=10000&project=heroes'''
+    r=requests.get(api_req.format(account_id))
+    matchhistory=r.json()
+    pickle.dump(matchhistory,open(str(account_id)+'_matches.p','wb'))
+    return matchhistory
 
+# load the json object saved
+matchhistory=pickle.load(open(filepath,'rb'))
 
+def parse_player_match_history(matchhistory):
+    '''
+    Parses the json data from the match history of a player.
 
-## parse open dota data
-match_ids=[]
-heroes=[]
-match_length=[]
-win=[]
-team_radiant=[] # player_slot < 128 is radiant
-l=len(matchhistory)
-n=0
-for match in matchhistory:
-    match_ids.append(match['match_id'])
-    playersheroes=list(match['heroes'].values())
-    for d in playersheroes:
-        try:
-            if d['account_id']==account_id:
-                break
-        except KeyError:
-            pass
-    heroes.append(d['hero_id'])
-    match_length.append(match['duration'])
-    win.append(True if match['radiant_win'] and match['player_slot']<128 else False)
-    team_radiant.append(True if match['player_slot']<128 else False)
-    n+=1
-    x=str(n)+'/'+str(l)
-    print(f'\r', end="")
-    print(f'{x}', end="")
-print(f'\n')
+    Input:
+    matchhistory: json dictionary type with the format of opendota
+    
+    Output:
+    player_df : pandas dataframe with the following schema
+    -- match_ids (int): the unique match id
+    -- heroes (int): the unique hero id 
+    -- match_length (int): match length in seconds
+    -- win (bool): True if player won
+    -- team_radiant(bool): True if player was Radiant 
 
-player_df=pd.DataFrame({'match_ids':match_ids,
-'heroes':heroes,
-'match_length':match_length,
-'win':win,
-'team_radiant':team_radiant})
+    '''
+    ## parse open dota data
+    match_ids=[]
+    heroes=[]
+    match_length=[]
+    win=[]
+    team_radiant=[] # player_slot < 128 is radiant
+    l=len(matchhistory)
+    n=0
+    for match in matchhistory:
+        match_ids.append(match['match_id'])
+        playersheroes=list(match['heroes'].values())
+        for d in playersheroes:
+            try:
+                if d['account_id']==account_id:
+                    break
+            except KeyError:
+                pass
+        heroes.append(d['hero_id'])
+        match_length.append(match['duration'])
+        win.append(True if match['radiant_win'] and match['player_slot']<128 else False)
+        team_radiant.append(True if match['player_slot']<128 else False)
+        n+=1
+        x=str(n)+'/'+str(l)
+        print(f'\r', end="")
+        print(f'{x}', end="")
+    print(f'\n')
 
+    player_df=pd.DataFrame({'match_ids':match_ids,
+    'heroes':heroes,
+    'match_length':match_length,
+    'win':win,
+    'team_radiant':team_radiant})
+    return player_df
