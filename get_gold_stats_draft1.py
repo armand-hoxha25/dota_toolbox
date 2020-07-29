c2048d6fdc140883c2caac84c179f716125f31b0
armand-hoxha25 <armand.hoxha25@gmail.com>
2020-07-29 03:52:20 -0400

    get gold and xp changes for every hero

--- get_gold_stats_draft1.py
+++ get_gold_stats_draft1.py
@@ -15,34 +15,76 @@ f=open(combatfile,'r')
 txt=f.readlines()
 f.close()
 matchinfo=parse_matchinfo(infofile)
-timestamp_gold=[]
-heroes=[]
-gold_change=[]
-experience_change=[]
-timestamp_xp=[]
-hero=matchinfo['hero_name'][0]
-m=1
-for l in txt:
-    # find the gold change, and append it
-    if (hero in l) and ('gold' in l):
-        s=l.find('looses')
-        if s==-1:
-            s=l.find('receives')
-            s=s+len('receives')
-            m=1
-        else:
-            s=s+len('looses')
-            m=-1
-        e=l.find('gold')
-        gold_change.append(m*int(l[s:e]))
-        timestamp_gold.append(l[1:13])
-        
+heroes_dict={}
+
+
+def str_time_to_sec(s):
+    """Convert the string time "HH:MM:SS.xxx" into total seconds.
+
+    Args:
+        s (str): string in the format mentioned above.
+
+    Returns:
+        float: floating point number of the total number of seconds.
+    """    
+    sec=float(s[6:])
+    min_to_sec=60*float(s[3:5])
+    hr_to_sec=3600*float(s[:2])
     
-    if (hero in l) and ('XP' in l):
-        s=l.find('gains')
-        s=s+len('gains')
-        e=l.find('XP')
-        experience_change.append(int(l[s:e]))
-        timestamp_xp.append(l[1:13])        
+    return sec+min_to_sec+hr_to_sec
 
 
+matchstart=None
+for hero in matchinfo['hero_name']:
+    m=1
+    
+    timestamp_gold=[]
+    heroes=[]
+    gold_change=[]
+    experience_change=[]
+    timestamp_xp=[] 
+    
+    for l in txt:
+        # find the gold change, and append it
+        if (hero in l) and ('gold' in l):
+            s=l.find('looses')
+            if s==-1:
+                s=l.find('receives')
+                s=s+len('receives')
+                m=1
+            else:
+                s=s+len('looses')
+                m=-1
+            e=l.find('gold')
+            gold_change.append(m*int(l[s:e]))
+            timestamp_gold.append(str_time_to_sec(l[1:13]))
+            
+        
+        if (hero in l) and ('XP' in l):
+            s=l.find('gains')
+            s=s+len('gains')
+            e=l.find('XP')
+            experience_change.append(int(l[s:e]))
+            timestamp_xp.append(str_time_to_sec(l[1:13]))    
+
+        if l[0]=='[':
+            if matchstart==None:
+                matchstart=str_time_to_sec(l[1:13])
+            else:
+                match_length=str_time_to_sec(l[1:13])
+
+    timestamp_gold=timestamp_gold+ np.linspace(matchstart,match_length,num=int(match_length-matchstart)).tolist()
+    gold_change=gold_change+np.ones((1,int(match_length-matchstart))).tolist()[0]
+    heroes_dict[hero]={
+    'timestamp_gold':timestamp_gold,
+    'gold_change':gold_change,
+    'experience_change':experience_change,
+    'timestamp_xp':timestamp_xp
+    }
+
+df=pd.DataFrame({'gold_change':heroes_dict['npc_dota_hero_mirana']['gold_change'],\
+                 'timestamp':heroes_dict['npc_dota_hero_mirana']['timestamp_gold']})
+
+df=df.sort_values(by='timestamp')
+df.plot(x='timestamp',y='gold_change')
+plt.show()
