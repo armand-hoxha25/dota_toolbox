cefe723b277a963bca84686c984413d99c8da66c
armand hoxha <armand.hoxha25@gmail.com>
2020-08-10 23:34:59 -0400

    now entering the form sends somewhere

--- webapp_main.py
+++ webapp_main.py
@@ -1,10 +1,33 @@
-from flask import Flask, redirect, url_for, render_template
+import os
+from flask import Flask, redirect, url_for, render_template, request
+static_dir=os.path.abspath('./templates')
+app=Flask(__name__,template_folder=static_dir,static_folder=static_dir)
 
-app=Flask(__name__)
-
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def home():
+    if request.method== "POST":
+        matchid=request.form["matchid"]
+        with open("tmp_txt.txt","w+") as f:
+            f.write("OMG!!!")
+        print("-----------matchid=",matchid,'------------------------')
+        return  f"<h1>{matchid}</h1>"
+    else:
+        return render_template("index.html", template_folder='templates')
     return render_template("index.html", template_folder='templates')
+
+@app.route("/match",methods=["POST","GET"])
+def get_match():
+    if request.method== "GET":
+        matchid=request.form["matchid"]
+        with open("tmp_txt.txt","w+") as f:
+            f.write("OMG!!!")
+        print("-----------matchid--------------------")
+        return  f"<h1>{matchid}</h1>"
+    else:
+        return render_template("index_dash.html")
+@app.route("/<matchid>")
+def match(matchid):
+    return f"<h1>{matchid}</h1>"
 
 if __name__== "__main__":
     app.run()
