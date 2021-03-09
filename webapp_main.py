bffa070a8c6bf471a83ac49c6851d6f77a8e3444
armand <armand.hoxha25@gmail.com>
2021-03-08 21:38:22 -0500

    Added sample graph to the page following matchid

--- webapp_main.py
+++ webapp_main.py
@@ -1,5 +1,10 @@
 import os
 from flask import Flask, redirect, url_for, render_template, request
+import plotly.express as px
+import plotly
+import plotly.graph_objects as go
+import json
+
 static_dir=os.path.abspath('./templates')
 app=Flask(__name__,template_folder=static_dir,static_folder=static_dir)
 
@@ -8,26 +13,33 @@ def home():
     if request.method== "POST":
         matchid=request.form["matchid"]
         with open("tmp_txt.txt","w+") as f:
-            f.write("OMG!!!")
+            f.write(matchid)
         print("-----------matchid=",matchid,'------------------------')
-        return  render_template("index_dash.html")
+        graphJSON=get_graphs(matchid)
+        return  render_template("index_dash.html",matchid=matchid,graphJSON=graphJSON)
     else:
         return render_template("index.html", template_folder='templates')
-    return render_template("index.html", template_folder='templates')
+    return render_template("index.html", template_folder='templates', matchid=matchid)
 
 @app.route("/match",methods=["POST","GET"])
 def get_match():
     if request.method== "GET":
         matchid=request.form["matchid"]
         with open("tmp_txt.txt","w+") as f:
-            f.write("OMG!!!")
+            f.write(matchid)
         print("-----------matchid--------------------")
         return  f"<h1>{matchid}</h1>"
     else:
-        return render_template("index_dash.html")
+        return render_template("index_dash.html",matchid=matchid)
 @app.route("/<matchid>")
 def match(matchid):
     return f"<h1>{matchid}</h1>"
+
+def get_graphs(matchid):
+    data=[go.Scatter(x=[1,2,3],y=[1,2,3])]
+    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+    print('graph generated')
+    return graphJSON
 
 if __name__== "__main__":
     app.run()
